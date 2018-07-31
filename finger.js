c54bd947ba17e620f4076d8d75f034c48000101f
Xi Yu <xixi5251@gmail.com>
2018-07-31 14:13:07 +0000

    All done

--- finger.js
+++ finger.js
@@ -14,3 +14,17 @@ var rightRoamingFingers = 10 - right;
 
 console.log(leftJoinedFingers, leftRoamingFingers);
 console.log(rightJoinedFingers, rightRoamingFingers);
+
+//Join all the fingers that represent the 2 numbers being mutiplied
+var joinedFingers = leftJoinedFingers + rightJoinedFingers;
+
+//then count how many fingers there are joined . *10
+
+joinedFingers *= 10
+console.log('Joined', joinedFingers);
+//lastly count roaming fingers from left hand and multiply by roaming fingers in right hand
+var roammingFingers = leftRoamingFingers + rightRoamingFingers;
+console.log('Roaming ', roammingFingers);
+
+//finaly step: add step #4 with step #5
+console.log('FInal: ', joinedFingers + roammingFingers);
