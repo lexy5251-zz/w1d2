0f1a7557cfeae62b7ab1ea0c4fb592a72dfb9bdd
Xi Yu <xixi5251@gmail.com>
2018-07-31 13:42:47 +0000

    Step 2... mapping

--- finger.js
+++ finger.js
@@ -4,3 +4,13 @@ var right = process.argv[3];
 console.log('Left ', left);
 console.log('Right ', right);
 // right text commit
+
+
+
+var leftJoinedFingers = left - 5;
+var rightJoinedFingers = right - 5;
+var leftRoamingFingers = 10 - left;
+var rightRoamingFingers = 10 - right;
+
+console.log(leftJoinedFingers, leftRoamingFingers);
+console.log(rightJoinedFingers, rightRoamingFingers);
